574f2735a1e185071f1612c65c1bac50c4e57975
hill <hill@hilll.dev>
2026-09-03 09:19:30 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "BritishMuseum3DScans";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.blender
+          pkgs.meshlab
+        ];
+      };
+    });
+}
